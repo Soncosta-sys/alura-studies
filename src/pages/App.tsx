1da2100033ad76e4df83e0473a23ccdbbe655450
Talison Medeiros <talisonmedeiros92@gmail.com>
2022-05-16 07:42:30 -0300

    Finalizado Primeiro Capítulo

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Cronometro } from '../components/cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import Itarefa from '../types/tarefa';
 import style from './style.module.scss';
 
 
 function App() {
+  const [tarefas, setTarefas] = useState<Itarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<Itarefa>();
+
+function selecionaTarefa(tarefaSelecionada: Itarefa){
+  setSelecionado(tarefaSelecionada);
+  setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa =>({
+    ...tarefa,
+    selecionado: tarefa.id === tarefaSelecionada.id ? true : false || tarefa.id === tarefaSelecionada.id  ? false : true 
+
+  })))
+}
+
   return (
     <div className={style.AppStyle}>
-     <Formulario />
-     <Lista />
+     <Formulario setTarefas={setTarefas} />
+     <Lista 
+     tarefas={tarefas} 
+     selecionaTarefa={selecionaTarefa}
+     />
      <Cronometro />
     </div>
   );
